801b60e7e252134696a489407a8de6a0612cb665
AnoopArpan <anooparpan027@gmail.com>
2025-06-18 21:19:43 +0530

    basic buttons

---
+++ main page/script.js
@@ -0,0 +1,19 @@
+// Update these URLs to your actual service pages
+document.getElementById('findMedicines').onclick = function() {
+  window.location.href = 'medicines.html';
+};
+document.getElementById('checkSymptoms').onclick = function() {
+  window.location.href = 'symptoms.html';
+};
+document.getElementById('diseasesCures').onclick = function() {
+  window.location.href = 'diseases.html';
+};
+document.getElementById('bookRoom').onclick = function() {
+  window.location.href = 'bookroom.html';
+};
+document.getElementById('callAmbulance').onclick = function() {
+  window.location.href = 'ambulance.html';
+};
+document.getElementById('compareMeds').onclick = function() {
+  window.location.href = 'comparemeds.html';
+};
